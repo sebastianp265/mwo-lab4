47bddb00371091db06bdb5d863cb8b46578cbf94
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2023-11-30 20:43:18 +0100

    init spring project

---
+++ src/main/java/com/example/mwolab4/MwoLab4Application.java
@@ -0,0 +1,13 @@
+package com.example.mwolab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwoLab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwoLab4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mwolab4/MwoLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mwolab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwoLab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
